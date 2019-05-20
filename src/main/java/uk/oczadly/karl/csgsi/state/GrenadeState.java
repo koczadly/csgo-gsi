c97ca2c9d8cd702ab743a4ab884322f3280da199
Karl Oczadly <karl@oczadly.uk>
2019-05-21 00:56:30 +0100

    Add grenade state

---
+++ src/main/java/uk/oczadly/karl/csgsi/state/GrenadeState.java
@@ -0,0 +1,93 @@
+package uk.oczadly.karl.csgsi.state;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+import uk.oczadly.karl.csgsi.state.components.Coordinate;
+
+public class GrenadeState {
+    
+    @Expose
+    @SerializedName("owner")
+    private String ownerId;
+    
+    @Expose
+    @SerializedName("position")
+    private Coordinate position;
+    
+    @Expose
+    @SerializedName("velocity")
+    private Coordinate velocity;
+    
+    @Expose
+    @SerializedName("lifetime")
+    private Double lifetime;
+    
+    @Expose
+    @SerializedName("type")
+    private Type type;
+    
+    @Expose
+    @SerializedName("effecttime")
+    private Double effectTime;
+    
+    
+    /**
+     * @return the ID of the player who threw the grenade
+     */
+    public String getOwnerId() {
+        return ownerId;
+    }
+    
+    /**
+     * @return the current position of the grenade on the map
+     */
+    public Coordinate getPosition() {
+        return position;
+    }
+    
+    /**
+     * @return the velocity of the grenade
+     */
+    public Coordinate getVelocity() {
+        return velocity;
+    }
+    
+    /**
+     * @return the current age of the grenade in seconds
+     */
+    public Double getLifetime() {
+        return lifetime;
+    }
+    
+    /**
+     * @return the type of grenade
+     */
+    public Type getType() {
+        return type;
+    }
+    
+    /**
+     * @return the number of seconds left until the effect ends
+     */
+    public Double getEffectTime() {
+        return effectTime;
+    }
+    
+    
+    
+    public enum Type {
+        @SerializedName("smoke")
+        SMOKE,
+        @SerializedName("decoy")
+        DECOY,
+        @SerializedName("inferno")
+        INCENDIARY,
+        @SerializedName("firebomb")
+        MOLOTOV,
+        @SerializedName("flashbang")
+        FLASHBANG,
+        @SerializedName("frag")
+        FRAG;
+    }
+
+}
