1697bbf33988db2432420cfd4cfd348b03282cd4
Karl Oczadly <karl@oczadly.uk>
2019-05-22 22:17:23 +0100

    Add countdown state

---
+++ src/main/java/uk/oczadly/karl/csgsi/state/PhaseCountdownState.java
@@ -0,0 +1,51 @@
+package uk.oczadly.karl.csgsi.state;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class PhaseCountdownState {
+    
+    @Expose
+    @SerializedName("phase")
+    private Phase phase;
+    
+    @Expose
+    @SerializedName("phase_ends_in")
+    private Double timer;
+    
+    
+    /**
+     * @return the currently ongoing countdown phase
+     */
+    public Phase getPhase() {
+        return phase;
+    }
+    
+    /**
+     * @return the remaining time of the phase in seconds
+     */
+    public Double getRemainingTime() {
+        return timer;
+    }
+    
+    
+    
+    public enum Phase {
+        /** Game currently in progress. */
+        @SerializedName("live")
+        LIVE,
+    
+        /** Game in progress and bomb has been planted. */
+        @SerializedName("bomb")
+        BOMB,
+    
+        /** Bomb is currently being defused. */
+        @SerializedName("defuse")
+        DEFUSE,
+        
+        /** Round is over. */
+        @SerializedName("over")
+        OVER;
+    }
+    
+}
