c62306c9363341feaf749c84b4fc9d5ba44e765c
Karl Oczadly <karl@oczadly.uk>
2019-05-28 00:06:16 +0100

    Add players state unit test

---
+++ src/test/java/uk/oczadly/karl/csgsi/state/state/AllPlayersStateTest.java
@@ -0,0 +1,71 @@
+package uk.oczadly.karl.csgsi.state.state;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import uk.oczadly.karl.csgsi.state.BombState;
+import uk.oczadly.karl.csgsi.state.GameState;
+import uk.oczadly.karl.csgsi.state.components.Coordinate;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+public class AllPlayersStateTest extends GameStateBaseTest {
+    
+    @Test
+    public void testSize() {
+        GameState state = deserilizeState("{\n" +
+                "  \"allplayers\": {\n" +
+                "    \"76561197960265729\": {\n" +
+                "      \"name\": \"Wesley\",\n" +
+                "      \"observer_slot\": 1,\n" +
+                "      \"team\": \"CT\",\n" +
+                "      \"match_stats\": {\n" +
+                "        \"kills\": 0,\n" +
+                "        \"assists\": 0,\n" +
+                "        \"deaths\": 0,\n" +
+                "        \"mvps\": 0,\n" +
+                "        \"score\": 0\n" +
+                "      },\n" +
+                "      \"position\": \"-1353.00, 645.00, 1614.03\",\n" +
+                "      \"forward\": \"0.29, -0.96, -0.03\",\n" +
+                "      \"state\": {\n" +
+                "        \"health\": 100,\n" +
+                "        \"armor\": 0,\n" +
+                "        \"helmet\": false,\n" +
+                "        \"flashed\": 0,\n" +
+                "        \"burning\": 0,\n" +
+                "        \"money\": 800,\n" +
+                "        \"round_kills\": 0,\n" +
+                "        \"round_killhs\": 0,\n" +
+                "        \"round_totaldmg\": 0,\n" +
+                "        \"equip_value\": 200\n" +
+                "      },\n" +
+                "      \"weapons\": {\n" +
+                "        \"weapon_0\": {\n" +
+                "          \"name\": \"weapon_knife\",\n" +
+                "          \"paintkit\": \"default\",\n" +
+                "          \"type\": \"Knife\",\n" +
+                "          \"state\": \"holstered\"\n" +
+                "        },\n" +
+                "        \"weapon_1\": {\n" +
+                "          \"name\": \"weapon_hkp2000\",\n" +
+                "          \"paintkit\": \"default\",\n" +
+                "          \"type\": \"Pistol\",\n" +
+                "          \"ammo_clip\": 13,\n" +
+                "          \"ammo_clip_max\": 13,\n" +
+                "          \"ammo_reserve\": 52,\n" +
+                "          \"state\": \"active\"\n" +
+                "        }\n" +
+                "      }\n" +
+                "    }\n" +
+                "  }\n" +
+                "}");
+        assertNotNull(state);
+        
+        assertEquals(1, state.getPlayerStates().size());
+        assertNotNull(state.getPlayerStates().get("76561197960265729"));
+        
+        assertEquals("Wesley", state.getPlayerStates().get("76561197960265729").getName());
+    }
+    
+}
