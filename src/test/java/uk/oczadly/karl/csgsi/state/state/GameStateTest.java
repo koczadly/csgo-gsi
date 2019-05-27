5d20811fc9a2561b76874e4d074bb0dde8c61da0
Karl Oczadly <karl@oczadly.uk>
2019-05-27 21:46:40 +0100

    Add GameState tests

---
+++ src/test/java/uk/oczadly/karl/csgsi/state/state/GameStateTest.java
@@ -0,0 +1,29 @@
+package uk.oczadly.karl.csgsi.state.state;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import uk.oczadly.karl.csgsi.state.GameState;
+
+public class GameStateTest extends GameStateBaseTest {
+    
+    /* Component tests are performed in their individual test classes. */
+    
+    
+    @Test
+    public void testEmpty() {
+        GameState state = deserilizeState("{}");
+        
+        assertNotNull(state.getAuthenticationTokens());
+        assertTrue(state.getAuthenticationTokens().isEmpty());
+        
+        assertNull(state.getBombState());
+        assertNull(state.getGrenadeStates());
+        assertNull(state.getMapState());
+        assertNull(state.getPhaseCountdownState());
+        assertNull(state.getPlayerState());
+        assertNull(state.getPlayerStates());
+        assertNull(state.getProviderDetails());
+        assertNull(state.getRoundState());
+    }
+    
+}
