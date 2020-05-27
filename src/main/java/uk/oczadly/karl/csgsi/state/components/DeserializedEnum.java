f139420ecd597efe70f773e726f6c85c0304075f
Karl Oczadly <karl@oczadly.uk>
2020-05-27 22:15:35 +0100

    Add class description and method javadocs for DeserializedEnum

--- src/main/java/uk/oczadly/karl/csgsi/state/components/DeserializedEnum.java
+++ src/main/java/uk/oczadly/karl/csgsi/state/components/DeserializedEnum.java
@@ -14,6 +14,15 @@ import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
 import java.util.Objects;
 
+/**
+ * This class is a wrapper for {@code Enum} values, allowing for cases where the corresponding enum constant cannot be
+ * parsed while still maintaining the original information.
+ * In most implementations, the {@link #getEnum()} method can be used to retrieve the enum value as normal. For cases
+ * where the originally returned value could not be parsed as an enum constant (resulting in a null enum value), the
+ * {@link #getRawString()} value will return the raw serialized value received from the game client.
+ * @param <E> the enum class
+ * @see #getEnum()
+ */
 @JsonAdapter(DeserializedEnum.DeserializerFactory.class)
 public class DeserializedEnum<E extends Enum<E>> {
     
@@ -27,6 +36,9 @@ public class DeserializedEnum<E extends Enum<E>> {
     
     
     /**
+     * Returns the parsed enum value, or null in cases where the corresponding enum constant could not be parsed. If
+     * the game client did not send a null value, and the value returned from this method is null, then the
+     * {@link #getRawString()} method will return the serialized string value.
      * @return the parsed enum value, or null if not found
      */
     public E getEnum() {
@@ -34,11 +46,14 @@ public class DeserializedEnum<E extends Enum<E>> {
     }
     
     /**
+     * Returns the raw string value sent by the game client. This method should be preferred for logging or storing
+     * game state data, as it will always contain the correct value sent by the game client.
      * @return the raw value sent by the game client
      */
     public String getRawString() {
         return rawVal;
     }
+    
     
     @Override
     public String toString() {
@@ -94,12 +109,12 @@ public class DeserializedEnum<E extends Enum<E>> {
             String val = in.nextString();
             
             E enumVal = enumAdapter.read(new JsonReader(new StringReader("\"" + val + "\"")));
-            return new DeserializedEnum<E>(enumVal, val);
+            return new DeserializedEnum<>(enumVal, val);
         }
         
         @Override
         public void write(JsonWriter out, DeserializedEnum<E> value) throws IOException {
-            throw new UnsupportedOperationException();
+            enumAdapter.write(out, value.getEnum());
         }
     }
 
