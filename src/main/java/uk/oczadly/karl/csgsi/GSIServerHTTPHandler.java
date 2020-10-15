131b9ec0746ef75b5b839a6f052cd1751817e747
Karl Oczadly <karl@oczadly.uk>
2020-10-15 14:48:08 +0100

    Change format of listening path in web panel.

--- src/main/java/uk/oczadly/karl/csgsi/GSIServerHTTPHandler.java
+++ src/main/java/uk/oczadly/karl/csgsi/GSIServerHTTPHandler.java
@@ -59,13 +59,13 @@ class GSIServerHTTPHandler implements HTTPRequestHandler {
         
         // Build HTML
         StringBuilder sb = new StringBuilder();
-        sb.append("<head><script src=\"https://cdn.jsdelivr.net/gh/google/code-prettify@master/loader/")
-                .append("run_prettify.js\"></script><meta charset=\"UTF-8\"></head>\n");
+        sb.append("<head><script src=\"https://cdnjs.cloudflare.com/ajax/libs/prettify/r298/run_prettify.js\">")
+                .append("</script><meta charset=\"UTF-8\"></head>\n");
         sb.append("<body><h1><a style=\"color:green\" href=\"").append(Util.GITHUB_URL)
                 .append("\">CSGO-GSI server is running!</a></h1>\n");
         // Listening port
-        sb.append("<b>Listening on:</b> <code>")
-                .append(gsi.getBindingAddress() != null ? gsi.getBindingAddress() : "localhost")
+        sb.append("<b>Listening on:</b> <code>http://")
+                .append(gsi.getBindingAddress() != null ? gsi.getBindingAddress() : "0.0.0.0")
                 .append(":").append(gsi.getPort()).append("</code><br>\n");
         // Uptime
         double uptimeMins = ((now - gsi.serverStartTimestamp.toEpochMilli()) / 1000d) / 60d;
